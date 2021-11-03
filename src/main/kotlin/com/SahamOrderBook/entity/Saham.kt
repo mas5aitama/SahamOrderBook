30caf2897fd760667457cbc81b9b7873f55606a9
Pandhuta Bangkit S <bangkitsamesta@gmail.com>
2021-11-03 19:45:23 +0700

    updated

--- src/main/kotlin/com/SahamOrderBook/entity/Saham.kt
+++ src/main/kotlin/com/SahamOrderBook/entity/Saham.kt
@@ -1,60 +1,58 @@
-package com.SahamOrderBook.apps.entity
+package com.SahamOrderBook.entity
 
 import javax.persistence.*
 import javax.validation.constraints.NotNull
 
-
 @Entity
 @Table(name = "saham")
 class Saham {
-
     @Id
     @NotNull
-    @Column
-    var kodeSaham: String = ""
+    @Column(nullable = false, length = 10)
+    var kodeSaham: String? = null
 
     @NotNull
-    @Column
-    var namaSaham: String? = ""
+    @Column(nullable = false, length = 250)
+    var namaSaham: String? = null
 
     @NotNull
-    @Column
+    @Column(nullable = false)
     var hargaTerakhir: Double? = null
 
     @NotNull
-    @Column
+    @Column(nullable = false)
     var change: Double? = null
 
     @NotNull
-    @Column
+    @Column(nullable = false)
     var changePersen: Double? = null
 
     @NotNull
-    @Column
+    @Column(nullable = false)
     var priceClose: Double? = null
 
     @NotNull
-    @Column
+    @Column(nullable = false)
     var priceOpen: Double? = null
 
     @NotNull
-    @Column
+    @Column(nullable = false)
     var priceHigh: Double? = null
 
     @NotNull
-    @Column
+    @Column(nullable = false)
     var priceLow: Double? = null
 
     @NotNull
-    @Column
+    @Column(nullable = false)
     var foreignSale: Double? = null
 
     @NotNull
-    @Column
+    @Column(nullable = false)
     var domesticBuy: Double? = null
 
     @OneToMany(cascade = arrayOf(CascadeType.ALL), orphanRemoval = true)
-    @JoinColumn(name = "kodeSaham")
+    @JoinColumn(name = "kode_saham")
     val orderbook: List<OrderBook>? = ArrayList<OrderBook>()
 
 }
