d2328996713e44eddffa4e5ed459501fff707a83
Pandhuta Bangkit S <bangkitsamesta@gmail.com>
2021-11-09 17:27:00 +0700

    Sy remove generate native, agar tdk berat di sequance

--- src/main/kotlin/com/pandhuta/sahamdemo/entity/User.kt
+++ src/main/kotlin/com/pandhuta/sahamdemo/entity/User.kt
@@ -5,15 +5,13 @@ import org.hibernate.annotations.CacheConcurrencyStrategy
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder
 import javax.persistence.*
 
-
 @Entity
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 @Table(name = "ms_user")
 class User {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    var id: Int? = 0
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    var id: Long? = 0
 
     @Column(unique = true, nullable = false)
     var email: String? = null
@@ -25,4 +23,5 @@ class User {
             val passwordEncoder = Argon2PasswordEncoder()
             field = passwordEncoder.encode(value)
         }
+
 }
