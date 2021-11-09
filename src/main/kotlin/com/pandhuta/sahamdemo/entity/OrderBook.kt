154460d6f9933d1117073649efe01ba438f6ecbc
Pandhuta Bangkit S <bangkitsamesta@gmail.com>
2021-11-09 16:59:59 +0700

    Karena ManyToOne, untuk POSTnya blm bisa. Jd sy mark Dahulu. Maka Jadi sy pakai OneToMany dan berhasil untuk CRUD.

--- src/main/kotlin/com/pandhuta/sahamdemo/entity/OrderBook.kt
+++ src/main/kotlin/com/pandhuta/sahamdemo/entity/OrderBook.kt
@@ -1,12 +1,15 @@
 package com.pandhuta.sahamdemo.entity
 
 import org.hibernate.Hibernate
+import org.hibernate.annotations.*
 import org.hibernate.annotations.Cache
-import org.hibernate.annotations.CacheConcurrencyStrategy
-import org.hibernate.annotations.OnDelete
-import org.hibernate.annotations.OnDeleteAction
 import java.util.*
 import javax.persistence.*
+import javax.persistence.CascadeType
+import javax.persistence.Entity
+import javax.persistence.Index
+import javax.persistence.Table
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 
@@ -17,54 +20,66 @@ import javax.validation.constraints.NotNull
         Index(name = "order_book_x1", columnList = "kode_saham")
     ]
 )
-class OrderBook {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    var id: Long? = null
+class OrderBook(
+
+    @get:[NotBlank(message = "Kelurahan.name.NotEmpty")]
+    @NotNull
+    @Column
+    var bidOrder: Double? = null,
 
     @NotNull
     @Column
-    var bidOrder: Double? = null
+    var bidLot: Double? = null,
 
     @NotNull
     @Column
-    var bidLot: Double? = null
+    var bidPrice: Double? = null,
 
     @NotNull
     @Column
-    var bidPrice: Double? = null
+    var offerPrice: Double? = null,
 
     @NotNull
     @Column
-    var offerPrice: Double? = null
+    var offerLot: Double? = null,
 
     @NotNull
     @Column
-    var offerLot: Double? = null
+    var offerOrder: Double? = null,
 
-    @NotNull
-    @Column
-    var offerOrder: Double? = null
-
-    @ManyToOne
+    /*@ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "kode_saham", nullable = false)
-    var saham: Saham? = null
+    var saham: Saham? = null*/
 
+/*    @ManyToOne(optional = false, cascade = [CascadeType.PERSIST])
+    @JoinColumn(name = "kode_saham", nullable = false)
+    var saham: Saham? = null*/
+) {
+    @Id
+    @GeneratedValue(generator = "uuid4")
+    @GenericGenerator(name = "uuid4", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "orderBookId", nullable = false)
+    var orderBookId: UUID? = null
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        if (javaClass != other?.javaClass) return false
+
         other as OrderBook
 
-        return id != null && id == other.id
+        if (bidOrder != other.bidOrder) return false
+        if (orderBookId != other.orderBookId) return false
+
+        return true
     }
 
-    override fun hashCode(): Int = 32 * Objects.hash(id)
+    override fun hashCode(): Int {
+        var result = bidOrder?.hashCode() ?: 0
+        result = 31 * result + (orderBookId?.hashCode() ?: 0)
+        return result
+    }
 
-    @Override
     override fun toString(): String {
-        return this::class.simpleName + "(id = $id , bidOrder = $bidOrder , bidLot = $bidLot , " +
-                "bidPrice = $bidPrice , offerPrice = $offerPrice , offerLot = $offerLot , " +
-                "offerOrder = $offerOrder )"
+        return "OrderBook(bidLot=$bidLot, orderBookId=$orderBookId)"
     }
 }
