402e9b57b17a74c2995fef0d0163537f78529850
Pandhuta Bangkit S <bangkitsamesta@gmail.com>
2021-11-10 20:46:45 +0700

    Revert dari Pak Fajar dan penambahan (nullable = false). Koreksi dr Pak Pak Fajar, mengenai Hibernate dan Serial.

--- src/main/kotlin/com/pandhuta/sahamdemo/entity/OrderBook.kt
+++ src/main/kotlin/com/pandhuta/sahamdemo/entity/OrderBook.kt
@@ -1,15 +1,12 @@
 package com.pandhuta.sahamdemo.entity
 
 import org.hibernate.Hibernate
-import org.hibernate.annotations.*
 import org.hibernate.annotations.Cache
+import org.hibernate.annotations.CacheConcurrencyStrategy
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import java.util.*
 import javax.persistence.*
-import javax.persistence.CascadeType
-import javax.persistence.Entity
-import javax.persistence.Index
-import javax.persistence.Table
-import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 
@@ -20,66 +17,56 @@ import javax.validation.constraints.NotNull
         Index(name = "order_book_x1", columnList = "kode_saham")
     ]
 )
-class OrderBook(
+class OrderBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    var id: Long? = null
 
-    @get:[NotBlank(message = "Kelurahan.name.NotEmpty")]
-    @NotNull
-    @Column
-    var bidOrder: Double? = null,
-
-    @NotNull
-    @Column
-    var bidLot: Double? = null,
 
     @NotNull
-    @Column
-    var bidPrice: Double? = null,
+    @Column(nullable = false)
+    var bidOrder: Double? = null
 
     @NotNull
-    @Column
-    var offerPrice: Double? = null,
+    @Column(nullable = false)
+    var bidLot: Double? = null
 
     @NotNull
-    @Column
-    var offerLot: Double? = null,
+    @Column(nullable = false)
+    var bidPrice: Double? = null
 
     @NotNull
-    @Column
-    var offerOrder: Double? = null,
+    @Column(nullable = false)
+    var offerPrice: Double? = null
 
-    /*@ManyToOne
+    @NotNull
+    @Column(nullable = false)
+    var offerLot: Double? = null
+
+    @NotNull
+    @Column(nullable = false)
+    var offerOrder: Double? = null
+
+    @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "kode_saham", nullable = false)
-    var saham: Saham? = null*/
+    var saham: Saham? = null
 
-/*    @ManyToOne(optional = false, cascade = [CascadeType.PERSIST])
-    @JoinColumn(name = "kode_saham", nullable = false)
-    var saham: Saham? = null*/
-) {
-    @Id
-    @GeneratedValue(generator = "uuid4")
-    @GenericGenerator(name = "uuid4", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(name = "orderBookId", nullable = false)
-    var orderBookId: UUID? = null
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
         other as OrderBook
 
-        if (bidOrder != other.bidOrder) return false
-        if (orderBookId != other.orderBookId) return false
-
-        return true
+        return id != null && id == other.id
     }
 
-    override fun hashCode(): Int {
-        var result = bidOrder?.hashCode() ?: 0
-        result = 31 * result + (orderBookId?.hashCode() ?: 0)
-        return result
-    }
+    override fun hashCode(): Int = 32 * Objects.hash(id)
 
+    @Override
     override fun toString(): String {
-        return "OrderBook(bidLot=$bidLot, orderBookId=$orderBookId)"
+        return this::class.simpleName + "(id = $id , bidOrder = $bidOrder , bidLot = $bidLot , " +
+                "bidPrice = $bidPrice , offerPrice = $offerPrice , offerLot = $offerLot , " +
+                "offerOrder = $offerOrder )"
     }
 }
