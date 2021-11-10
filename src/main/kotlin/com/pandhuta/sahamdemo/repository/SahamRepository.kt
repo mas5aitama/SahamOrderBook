5c78793380176d612ac37deff2e51299fe890e51
Pandhuta Bangkit S <bangkitsamesta@gmail.com>
2021-11-10 20:52:03 +0700

    Revert dari Pak Fajar

--- src/main/kotlin/com/pandhuta/sahamdemo/repository/SahamRepository.kt
+++ src/main/kotlin/com/pandhuta/sahamdemo/repository/SahamRepository.kt
@@ -3,8 +3,6 @@ package com.pandhuta.sahamdemo.repository
 import com.pandhuta.sahamdemo.entity.OrderBook
 import com.pandhuta.sahamdemo.entity.Saham
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import java.util.*
 
 
 /**
@@ -13,12 +11,9 @@ import java.util.*
  * @author Ahmad Fajar <ahmadfajar@gmail.com>
  * @since  03/11/2021, modified: 03/11/2021 22:48
  */
-@Repository
-interface SahamRepository : JpaRepository<Saham, String>{
+interface SahamRepository : JpaRepository<Saham, String>
 
+
+interface OrderBookRepository : JpaRepository<OrderBook, Long> {
+    fun findBySahamKodeSahamOrderByBidPriceDesc(code: String): List<OrderBook>
 }
-
-interface OrderBookRepository : JpaRepository<OrderBook, UUID> {
-    //fun findBySahamKodeSahamOrderByBidPriceDesc(code: String): List<OrderBook>
-}
-
